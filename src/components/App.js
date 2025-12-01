b93708944f311956dc32becf815f56ef776927c9
Gavin Karlsson <100855173+GavinKarlsson@users.noreply.github.com>
2025-12-01 19:32:17 +0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,53 @@
-import React, { Component } from 'react'
-import ReactCoinsList from './Coins/ReactCoinsList'
-import ReactCoinsDetail from './Coins/ReactCoinsDetail'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Layout } from 'antd'
+
+import ReactHeader from './Navigation/ReactHeader'
+import ReactFooter from './Navigation/ReactFooter'
+
 import ReactHome from './General/ReactHome'
 import ReactAbout from './General/ReactAbout'
-import ReactEmpty from './Empty/ReactEmpty'
-import ReactGlobal from './Global/ReactGlobal'
-import { Route } from 'react-router-dom'
-import { Layout } from 'antd'
-import ReactHeader from './Navigation/ReactHeader'
+import ReactCoinsList from './Coins/ReactCoinsList'
+import ReactCoinsDetail from './Coins/ReactCoinsDetail'
+import ReactTrendingCoins from './Coins/ReactTrendingCoins'
 import ReactExchangesList from './Exchanges/ReactExchangesList'
 import ReactExchangeRates from './ExchangeRates/ReactExchangeRates'
-import ReactTrendingCoins from './Coins/ReactTrendingCoins'
-import ReactFooter from './Navigation/ReactFooter'
-import './App.css'
+import ReactGlobal from './Global/ReactGlobal'
+import ReactEmpty from './Empty/ReactEmpty'
 import ReactAssetPlatforms from './AssetPlatforms/ReactAssetPlatforms'
 
-// [NOTE] React prefix used with React.Components in order to differentiate from Ant Design UI components.
+const App = () => {
+  const isMobile = window.innerWidth <= 768; // adjust breakpoint as needed
 
-export default class App extends Component {
-  render () {
-    return (
+  return (
+    <Router>
       <Layout style={{ minHeight: '100vh', lineHeight: '1.6rem' }}>
-        <ReactHeader/>
+        <ReactHeader />
         <Layout>
-          <Route path='/' exact component={ReactHome}/>
-          <Route path='/about' exact component={ReactAbout}/>
-          <Route path='/trending-coins' exact component={ReactTrendingCoins}/>
-          <Route path='/coins' exact component={ReactCoinsList}/>
-          <Route path='/coins/:coinId/' exact component={ReactCoinsDetail}/>
-          <Route path='/exchanges/list' exact component={ReactExchangesList}/>
-          <Route path='/global' exact component={ReactGlobal}/>
-          <Route path='/empty' exact component={ReactEmpty}/>
-          <Route path='/exchange-rates' exact component={ReactExchangeRates}/>
-          <Route path='/asset-platforms' exact component={ReactAssetPlatforms}/>
+          <Routes>
+            {/* Redirect root based on device */}
+            <Route path="/" element={
+              isMobile ? <Navigate to="/coins-mobile" /> : <Navigate to="/coins" />
+            }/>
+
+            {/* Regular routes */}
+            <Route path="/about" element={<ReactAbout />} />
+            <Route path="/trending-coins" element={<ReactTrendingCoins />} />
+            <Route path="/coins" element={<ReactCoinsList />} />
+            <Route path="/coins-mobile" element={<ReactCoinsList />} /> {/* optional mobile-specific */}
+            <Route path="/coins/:coinId" element={<ReactCoinsDetail />} />
+            <Route path="/exchanges/list" element={<ReactExchangesList />} />
+            <Route path="/global" element={<ReactGlobal />} />
+            <Route path="/empty" element={<ReactEmpty />} />
+            <Route path="/exchange-rates" element={<ReactExchangeRates />} />
+            <Route path="/asset-platforms" element={<ReactAssetPlatforms />} />
+          </Routes>
         </Layout>
-        <ReactFooter/>
+        <ReactFooter />
       </Layout>
-    )
-  }
+    </Router>
+  )
 }
+
+export default App
+
