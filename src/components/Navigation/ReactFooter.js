2650c8d506052c1514cc813cf02564364fd733b8
Gavin Karlsson <100855173+GavinKarlsson@users.noreply.github.com>
2025-12-01 18:53:03 +0700

    Update ReactFooter.js

--- src/components/Navigation/ReactFooter.js
+++ src/components/Navigation/ReactFooter.js
@@ -7,6 +7,6 @@ const { Footer } = Layout
 export default function ReactFooter () {
   return (
     <Footer style={footerStyle}>
-            Developed by <strong>Eth2ADA</strong>.<br></br>Built with <strong>React</strong> and fully open source.<br></br>Powered by <strong>Cardano</strong> API.</Footer>
+            Developed by <strong>Eth2ADA</strong>.<br></br>Built with <strong>React</strong> and fully open source.<br></br>Powered by <strong>Cardano</strong> magic internet money.</Footer>
   )
 }
